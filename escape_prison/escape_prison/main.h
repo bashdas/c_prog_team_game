8013a7adadeb6aecc296a9e6dba5258701626cff
GIRIBUIN <96866036+GIRIBUIN@users.noreply.github.com>
2024-05-11 23:59:41 +0900

    Update main.h

--- escape_prison/escape_prison/main.h
+++ escape_prison/escape_prison/main.h
@@ -32,32 +32,33 @@
 
 void Setcolor(WORD color);
 void gotoxy(int x, int y, const char* s);
-void title(void); //게임 시작화면 
-void reset(void); //게임을 초기화 
-void drawMap(void); // 게임판 테두리를 그림 
-// void load_menu(void); // 메뉴를 불러오는 함수.
+void title(void);				// 게임 시작화면 
+void reset(void);				// 게임을 초기화 
+void drawMap(void);				// 게임판 테두리를 그림 
 void reset(void);
 void gotoxy1(int x, int y);
-int menuDraw1(void);
-int keyControl1(void);
+int menuDraw1(void);			// BACK, y- 15(1~4) 반환
+int keyControl1(void);			// keyboard value 반환
 void slowPrint(const char* s, int x, int y);
 
 
 /* game */
-void gameDraw(void);
-void story(void);
-char modeSelect(void);
-void modeSelectMapDraw(int ctitle, int ceasy, int cnormal, int chard);
+#define HOME 5					// 스테이지 성공 또는 실패 시 메인으로 복귀
+int gameDraw(void);				// HOME 반환
+void titleStory(void);
+char modeSelect(void);			// EASY(0), NORMAL(1), HARD(1) 반환
+void modeSelectMapDraw(int ctitle, int ceasy, int cnormal, int chard);  // 컬러색 매개변수
 void easyStory(void);
 void normalStory(void);
 void hardStory(void);
-void easyMode(void);
-void normalMode(void);
-void hardMode(void);
+int easyMode(void);				// BACK, -1, HOME 반환
+int normalMode(void);			// BACK, -1, HOME 반환
+int hardMode(void);				// BACK, -1, HOME 반환
+int playerMove(void);			// BACK, -1, HOME 반환
 void gameMapDraw(void);
 
 /* gamerule */
-#define EASY 0;
-#define NORMAL 1;
-#define HARD 2;
+#define EASY 0
+#define NORMAL 1
+#define HARD 2
 void gameRulesDraw(void);

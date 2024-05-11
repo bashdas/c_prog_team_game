b8418d117fd0d3bc806ae64591ce9abd56ea7e27
GIRIBUIN <96866036+GIRIBUIN@users.noreply.github.com>
2024-05-12 03:18:20 +0900

    Update main.h

--- escape_prison/escape_prison/main.h
+++ escape_prison/escape_prison/main.h
@@ -18,7 +18,7 @@
 #define ESC 27
 
 // MAP 값
-#define MAP_X 3 // 3 2
+#define MAP_X 10 // 3 2
 #define MAP_Y 2
 #define MAP_WIDTH 39  // 30 20
 #define MAP_HEIGHT 26
@@ -33,9 +33,12 @@
 void Setcolor(WORD color);
 void gotoxy(int x, int y, const char* s);
 void title(void);				// 게임 시작화면 
-void reset(void);				// 게임을 초기화 
+void reset(void);				// 게임을 초기화
+void resetMapInner(void);		// 맵 테두리 내부만 지움
+void resetMapTitle(void);		// 목숨창만 지움
+void resetMapMain(void);		// 게임창만 지움
+void resetMapBottom(void);		// 상호작용창만 지움
 void drawMap(void);				// 게임판 테두리를 그림 
-void reset(void);
 void gotoxy1(int x, int y);
 int menuDraw1(void);			// BACK, y- 15(1~4) 반환
 int keyControl1(void);			// keyboard value 반환
@@ -62,3 +65,23 @@ void gameMapDraw(void);
 #define NORMAL 1
 #define HARD 2
 void gameRulesDraw(void);
+
+
+/*
+0	: 검은색
+1	: 파란색
+2	: 초록색
+3	: 청록색
+4	: 빨간색
+5	: 자주색
+6	: 노란색
+7	: 흰색
+8	: 회색
+9	: 밝은 파란색
+10	: 밝은 초록색
+11	: 밝은 청록색
+12	: 밝은 빨간색
+13	: 밝은 자주색
+14	: 밝은 노란색
+15	: 밝은 흰색
+*/

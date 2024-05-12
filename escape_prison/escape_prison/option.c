a60f99a998a35eb3bd9a2f580fbc3fe48c3a3c09
GIRIBUIN <96866036+GIRIBUIN@users.noreply.github.com>
2024-05-13 00:54:39 +0900

    Update option.c

--- escape_prison/escape_prison/option.c
+++ escape_prison/escape_prison/option.c
@@ -22,13 +22,13 @@ void initOption() {
 }
 
 // 사용자 입력 메뉴 반환
-int selectOption() {
-	int x = 24, y = 16;
+int selectOption(void) {
+	int x = (MAP_X + 2) * 2, y = MAP_Y + 14;
 	resetMapInner();
 	gotoxy1(MAP_X + (MAP_WIDTH - 9) / 2, MAP_Y + 14);
-	printf("◇  배경음악 설정  w(UP)");   // 선택하면 난이도 선택 화면으로 이동
+	printf("  배경음악 설정  w(UP)");   // 선택하면 난이도 선택 화면으로 이동
 	gotoxy1(MAP_X + (MAP_WIDTH - 9) / 2, MAP_Y + 15);
-	printf("   테마 설정      a(LEFT)");     // 난이도 별 실행 및 조작법 출력
+	printf("  테마 설정      a(LEFT)");     // 난이도 별 실행 및 조작법 출력
 	gotoxy1(MAP_X * 2, MAP_Y + 17);
 	printf("■      **** spacebar to select ****     ");
 
@@ -64,29 +64,29 @@ int selectOption() {
 }
 
 // 음악 설정 기능
-int setMusic() {
+int setMusic(void) {
 	resetMapInner();
 	int keyInput;
 	keyInput = selectMusic();
 	switch (keyInput) {
 	case 1:
-		printf("1번 실행됨\n");
+		printf("  1번 실행됨\n");
 		break;
 	case 2:
-		printf("2번 실행\n");
+		printf("  2번 실행\n");
 		break;
 	}
 	return BACK;
 }
 
 // 사용자 입력 노래 반환
-int selectMusic() {
-	int x = 24, y = 16;
+int selectMusic(void) {
+	int x = (MAP_X + 2) * 2, y = MAP_Y + 14;
 	resetMapInner();
 	gotoxy1(MAP_X + (MAP_WIDTH - 9) / 2, MAP_Y + 14);
-	printf("◇  1번음악  w(UP)");   // 선택하면 난이도 선택 화면으로 이동
+	printf("◇ 1번음악  w(UP)");   // 선택하면 난이도 선택 화면으로 이동
 	gotoxy1(MAP_X + (MAP_WIDTH - 9) / 2, MAP_Y + 15);
-	printf("   2번음악      a(LEFT)");     // 난이도 별 실행 및 조작법 출력
+	printf("  2번음악      a(LEFT)");     // 난이도 별 실행 및 조작법 출력
 	gotoxy1(MAP_X * 2, MAP_Y + 17);
 	printf("■      **** spacebar to select ****     ");
 
@@ -122,7 +122,7 @@ int selectMusic() {
 }
 
 // 테마 설정 기능
-int setTheme() {
+int setTheme(void) {
 	resetMapInner();
 	int keyInput;
 	keyInput = selectTheme();
@@ -138,8 +138,8 @@ int setTheme() {
 }
 
 // 사용자 입력 테마 반환
-int selectTheme() {
-	int x = 24, y = 16;
+int selectTheme(void) {
+	int x = (MAP_X + 2) * 2, y = MAP_Y + 14;
 	resetMapInner();
 	gotoxy1(MAP_X + (MAP_WIDTH - 9) / 2, MAP_Y + 14);
 	printf("◇  1번테마  w(UP)");   // 선택하면 난이도 선택 화면으로 이동

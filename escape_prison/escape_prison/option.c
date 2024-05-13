286ba8f9568f5768888c0c160061dc87a472faab
bashdas <125852968+bashdas@users.noreply.github.com>
2024-05-13 19:03:17 +0900

    Update 주석 수정

--- escape_prison/escape_prison/option.c
+++ escape_prison/escape_prison/option.c
@@ -26,9 +26,9 @@ int selectOption(void) {
 	int x = (MAP_X + 2) * 2, y = MAP_Y + 14;
 	resetMapInner();
 	gotoxy1(MAP_X + (MAP_WIDTH - 9) / 2, MAP_Y + 14);
-	printf("  배경음악 설정  w(UP)");   // 선택하면 난이도 선택 화면으로 이동
+	printf("  배경음악 설정  w(UP)");   
 	gotoxy1(MAP_X + (MAP_WIDTH - 9) / 2, MAP_Y + 15);
-	printf("  테마 설정      a(LEFT)");     // 난이도 별 실행 및 조작법 출력
+	printf("  테마 설정      a(LEFT)");  
 	gotoxy1(MAP_X * 2, MAP_Y + 17);
 	printf("■      **** spacebar to select ****     ");
 
@@ -70,11 +70,11 @@ int setMusic(void) {
 	keyInput = selectMusic();
 	switch (keyInput) {
 	case 1:
-		printf("  1번 실행됨\n");
+		printf("  1번 실행됨\n");// 첫 번째 브금 실행// 첫 번째 브금 실행
 		break;
 	case 2:
-		printf("  2번 실행\n");
-		break;
+		printf("  2번 실행\n");// 두 번째 브금 실행문 추가 예정
+		// 브금 종류 추가 시 case 추가 후 select Music에 3번 음악 목록 추가
 	}
 	return BACK;
 }
@@ -84,9 +84,11 @@ int selectMusic(void) {
 	int x = (MAP_X + 2) * 2, y = MAP_Y + 14;
 	resetMapInner();
 	gotoxy1(MAP_X + (MAP_WIDTH - 9) / 2, MAP_Y + 14);
-	printf("◇ 1번음악  w(UP)");   // 선택하면 난이도 선택 화면으로 이동
+	printf("◇ 1번음악  w(UP)");   
 	gotoxy1(MAP_X + (MAP_WIDTH - 9) / 2, MAP_Y + 15);
-	printf("  2번음악      a(LEFT)");     // 난이도 별 실행 및 조작법 출력
+	printf("  2번음악      a(LEFT)");    
+
+
 	gotoxy1(MAP_X * 2, MAP_Y + 17);
 	printf("■      **** spacebar to select ****     ");
 
@@ -128,11 +130,12 @@ int setTheme(void) {
 	keyInput = selectTheme();
 	switch (keyInput) {
 	case 1:
-		printf("1번 실행됨\n");
+		printf("1번 실행됨\n");// 첫 번째 테마 실행문 추가 예정
 		break;
 	case 2:
-		printf("2번 실행\n");
+		printf("2번 실행\n");// 두 번째 테마 실행문 추가 예정
 		break;
+		// 테마 종류 추가 시 case 추가 후 selectTheme에 3번 음악 목록 추가
 	}
 	return BACK;
 }
@@ -142,9 +145,9 @@ int selectTheme(void) {
 	int x = (MAP_X + 2) * 2, y = MAP_Y + 14;
 	resetMapInner();
 	gotoxy1(MAP_X + (MAP_WIDTH - 9) / 2, MAP_Y + 14);
-	printf("◇  1번테마  w(UP)");   // 선택하면 난이도 선택 화면으로 이동
+	printf("◇  1번테마  w(UP)");   
 	gotoxy1(MAP_X + (MAP_WIDTH - 9) / 2, MAP_Y + 15);
-	printf("   2번테마     a(LEFT)");     // 난이도 별 실행 및 조작법 출력
+	printf("   2번테마     a(LEFT)");    
 	gotoxy1(MAP_X * 2, MAP_Y + 17);
 	printf("■      **** spacebar to select ****     ");
 

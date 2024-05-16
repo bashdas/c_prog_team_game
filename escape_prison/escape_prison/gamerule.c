cc93e943398a17229e6de876b117b0f4d7197808
GIRIBUIN <96866036+GIRIBUIN@users.noreply.github.com>
2024-05-17 00:16:01 +0900

    Update gamerule.c

--- escape_prison/escape_prison/gamerule.c
+++ escape_prison/escape_prison/gamerule.c
@@ -4,51 +4,20 @@
 void gameRulesDraw(void) {
 	int x = 26, y = 8;
 	resetMapInner();
-    Setcolor(4);
-    gotoxy(MAP_X + (MAP_WIDTH - 14) / 2, MAP_Y + 2, "┌--------------------------┐");
-    gotoxy(MAP_X + (MAP_WIDTH - 14) / 2, MAP_Y + 3, "|       GMAE    RULES      |");
-    gotoxy(MAP_X + (MAP_WIDTH - 14) / 2, MAP_Y + 4, "└--------------------------┘");
-    Setcolor(7);
-    gotoxy(MAP_X + 2, MAP_Y + 6,  "◇  ↑(w)  : Move to the Upward   ");
-    gotoxy(MAP_X + 2, MAP_Y + 8,  "   ↓(s)  : Move to the Downward ");
-    gotoxy(MAP_X + 2, MAP_Y + 10, "   ←(a)  : Move to the Left     ");
-    gotoxy(MAP_X + 2, MAP_Y + 12, "   →(d)  : Move to the Right    ");
-    gotoxy(MAP_X + 2, MAP_Y + 14, "   P     : Pause to the Game    ");
-    gotoxy(MAP_X + 2, MAP_Y + 16, "   Q     : Back to the Menu     ");
-    gotoxy(MAP_X + 2, MAP_Y + 18, "   ESC   : Quit to the Program  ");
-	gotoxy(MAP_X + 2, MAP_Y + 20, "   ENTER, SPACE : Skip or Submit");
-    gotoxy(MAP_X + 2, MAP_Y + 23, "   PRESS \"Q\" or \"q\" TO BACK ");
-	while (1)
-	{
-		
-		int n = keyControl1();
-		switch (n) {
-		case UP: {
-			if (y > MAP_Y + 6) {
-				gotoxy1((MAP_X + 2) * 2, y);
-				printf(" ");
-				gotoxy1((MAP_X + 2) * 2, y = y - 2);
-				printf("◇\b");
-			}
-			break;
-		}
-		case DOWN: {
-			if (y < MAP_Y + 20) {
-				gotoxy1((MAP_X + 2) * 2, y);
-				printf(" ");
-				gotoxy1((MAP_X + 2) * 2, y= y+2);
-				printf("◇\b");
-			}
-			break;
-		}
-		/*
-		case SUBMIT: {
-			return y - 7;
-		}*/
-		case BACK: {
-			break;
-		}
-		} // switch
-		if (n == BACK) break;
-	}
+	Setcolor(4);
+	gotoxy(MAP_X + (MAP_WIDTH - 14) / 2, MAP_Y + 2, "┌--------------------------┐");
+	gotoxy(MAP_X + (MAP_WIDTH - 14) / 2, MAP_Y + 3, "|       GMAE    RULES      |");
+	gotoxy(MAP_X + (MAP_WIDTH - 14) / 2, MAP_Y + 4, "└--------------------------┘");
+	Setcolor(7);
+	gotoxy(MAP_X + (MAP_WIDTH - 14) / 2, MAP_Y + 6, "↑(w)  : Move to the Upward   ");
+	gotoxy(MAP_X + (MAP_WIDTH - 14) / 2, MAP_Y + 8, "↓(s)  : Move to the Downward ");
+	gotoxy(MAP_X + (MAP_WIDTH - 14) / 2, MAP_Y + 10, "←(a)  : Move to the Left     ");
+	gotoxy(MAP_X + (MAP_WIDTH - 14) / 2, MAP_Y + 12, "→(d)  : Move to the Right    ");
+	gotoxy(MAP_X + (MAP_WIDTH - 14) / 2, MAP_Y + 14, "Q     : Back to the Menu     ");
+	gotoxy(MAP_X + (MAP_WIDTH - 14) / 2, MAP_Y + 16, "ESC   : Quit to the Program  ");
+	gotoxy(MAP_X + (MAP_WIDTH - 14) / 2, MAP_Y + 18, "ENTER, SPACE : Skip or Submit");
+	Setcolor(2);
+	gotoxy(MAP_X + (MAP_WIDTH - 14) / 2, MAP_Y + 22, "PRESS \"Q\" or \"q\" TO BACK");
+	Setcolor(7);
+	while (keyControl1() != BACK);
 }

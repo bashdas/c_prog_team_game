da4d3a2dd32dd38a8f42a716afe52e01c40b2236
202311301bae <ladolcevita1158@gmail.com>
2024-05-24 20:24:19 +0900

    Update gameplay_player&objects.h

--- escape_prison/escape_prison/gameplay_player&objects.h
+++ escape_prison/escape_prison/gameplay_player&objects.h
@@ -8,7 +8,6 @@
 #define MAP_X 3  // ?? main.h와 한번 더 비교해서 확인해보기 ?? 뭔가 예상했던 값과 다르다.
 
 
-
 // 게임 시스템
 
 int speed; //게임 속도 
@@ -18,21 +17,27 @@ int last_score = 0; //마지막 점수 저장  --reset함수에 의해 초기화
 int dir; //이동방향 저장 
 int key; //입력받은 키 저장 
 int status_on = 0; // 개발자용 status 표시활성화 변수.. 게임중에 S키를 누르면 활성 
-int length; //
-int length1=4; //
-int length2 =4;
+
+// 길이
+int length; // 플레이어의 꼬리
+int length1=4;   // strider의 길이
+int length2 =4;  // strider1의 길이
 
 
-// 체력
+// 체력 [전역변수]
 int HP = 3;
 int* pHP = &HP;
 
 // 플레이어의 좌표
 int x[100], y[100]; 
 
-// 아이템A의 좌표
-int x1[100], y1[100];
-int x2[100], y2[100];
+// 속도 증가 아이템의 좌표
+int x1[100], y1[100];  // item0 게임속도 증가 +
+int x2[100], y2[100];  // item0a 게임속도 증가 +
+
+
+
+
 
 
 

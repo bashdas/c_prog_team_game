fcba5556f361341151f03bdae6a824b510bba60f
GIRIBUIN <96866036+GIRIBUIN@users.noreply.github.com>
2024-05-24 20:08:01 +0900

    Update playersight4.c

--- playersight4.c
+++ playersight4.c
@@ -13,9 +13,88 @@ void getCursorPos(int* x, int* y) {
     }
 }
 
+void drawChar(int x, int y) {
+    gotoxy1(x, y);
+    printf("* * * * * * *");
+    gotoxy1(x, y+1);
+    printf("*           *");
+    gotoxy1(x, y + 2);
+    printf("*           *");
+    gotoxy1(x, y + 3);
+    printf("*     ★     *");
+    gotoxy1(x, y + 4);
+    printf("*           *");
+    gotoxy1(x, y + 5);
+    printf("*           *");
+    gotoxy1(x, y + 6);
+    printf("* * * * * * *");
+}
+
+void removeChar(int x, int y) {
+    gotoxy1(x, y);
+    printf("             ");
+    gotoxy1(x, y+1);
+    printf("             ");
+    gotoxy1(x, y+2);
+    printf("             ");
+    gotoxy1(x, y+3);
+    printf("             ");
+    gotoxy1(x, y+4);
+    printf("             ");
+    gotoxy1(x, y+5);
+    printf("             ");
+    gotoxy1(x, y+6);
+    printf("             ");
+}
+
+void movechar() {
+    int x = MAP_X * 2 + 1, y = MAP_Y + 3;
+    drawChar(x,y);
+    while (1)
+    {
+        int n = keyControl1();
+        switch (n) {
+        case UP: {
+            if (y > MAP_Y + 3) {
+                removeChar(x,y);
+                drawChar(x,--y);
+            }
+            break;
+        }
+        case DOWN: {
+            if (y < MAP_Y + 20) {
+                removeChar(x, y);
+                drawChar(x, ++y);
+            }
+            break;
+        }
+        case LEFT1: {
+            if (//x범위 ) {
+                removeChar(x, y);
+                drawChar(--x, y);
+            }
+            break;
+        }
+        case RIGHT1: {
+            if () {
+                removeChar(x, y);
+                drawChar(++x, y);
+            }
+            break;
+        }
+        case BACK:
+            return BACK;
+        case SUBMIT: {
+            return y - 15;
+        }
+        }
+    }
+}
+
 // 맵을 출력하는 함수
 void printMap(char map[MAP_HEIGHT][MAP_WIDTH], int cursorX, int cursorY) {
-    system("cls"); // 화면을 지우기
+    // system("cls"); // 화면을 지우기
+    resetMapMain();
     for (int i = 0; i < MAP_HEIGHT; i++) {
         for (int j = 0; j < MAP_WIDTH; j++) {
             // 커서 위치에 '★' 출력
@@ -85,7 +164,8 @@ int main() {
 
         char input = _getch();
         if (input == BACK) return BACK;
-        moveCursor(&cursorX, &cursorY, input);
+        //moveCursor(&cursorX, &cursorY, input);
+        movechar();
     }
 
     return 0;

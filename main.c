fa8dfb0930559eba21d9d44a2e1b83a92403a56c
202311301bae <ladolcevita1158@gmail.com>
2024-06-08 11:34:48 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,32 @@
+#include "main.h"
+//int x[100], y[100]; //x,y 좌표값을 저장 총 100개 
+//int key; //입력받은 키 저장 
+int speed; //게임 속도 
+int theme = 7;
+
+
+int main(void) {
+	setlocale(LC_ALL, "korean"); // 한국어 코드 페이지
+	int menuCode = 0;
+	int gameCode = 0;
+	srand((unsigned int)time(NULL));
+	//system("mode con cols=100 lines=30"); //콘솔창 크기 조절
+	while (1) {
+		title();
+		do {
+			gameCode = 0;
+			menuCode = menuDraw1(); // y - 15 = 1 ~ 4
+			if (menuCode == GAMESTART) {
+				gameCode = gameDraw();
+			}
+			else if (menuCode == GAMERULE) gameRulesDraw();
+			else if (menuCode == OPTION) initOption();
+			else if (menuCode == ITEM_EX) item_ex();
+			else if (menuCode == GAMEEXIT) {
+				system("cls");
+				gameExit();
+			}
+		} while (menuCode != BACK || gameCode == HOME);
+	}
+	return 0;
+}
---
+++ portal.c
@@ -0,0 +1,85 @@
+#include "main.h"
+
+void draw_portal(void)
+{
+	gotoxy1(22, 20);
+	Setcolor(2);
+	printf("P");
+	gotoxy1(80, 4);
+	printf("P");
+	Setcolor(15);
+}
+
+void remove_portal(void) {
+	gotoxy1(22, 21);
+	Setcolor(15);
+	printf("*");
+	gotoxy1(80, 4);
+	Setcolor(15);
+	printf("*");
+}
+
+void function_portal(int* x, int* y, int n)
+{
+	
+	if (*x == 23 && *y == 20 && n==LEFT)
+	{
+		int a = minigame(); // crash 1. pass 0
+		if (a == 0)
+		{
+			*x = 81;
+			*y = 5;
+			removePlayer(23, 20);
+			timek -= 1;
+			printf(" ");
+		}
+	
+		
+	
+		
+	}
+	if (*x == 80 && *y == 5 && n == UP)
+	{
+		int a = minigame(); // crash 1. pass 0
+		if (a == 0)
+		{
+			*x = 23;
+			*y = 21;
+			removePlayer(80, 5);
+			timek -= 1;
+			printf(" ");
+		}
+		
+		
+		
+	}//gameMapDraw();draw_portal();
+	
+
+	
+}
+
+void function_portal_Hard(int* x, int* y, int n)
+{
+	if (*x == 23 && *y == 20 && n==LEFT)
+	{
+		*x = 81;
+		*y = 5;
+		gotoxy1(23, 21);
+		resetMapInner();
+		gameMapDraw();
+		draw_portal();
+		timek -= 1;
+		printf(" ");
+	}
+	if (*x == 80 && *y == 5 && n == UP)
+	{
+		*x = 24;
+		*y = 21;
+		gotoxy1(80,5);
+		resetMapInner();
+		gameMapDraw();
+		draw_portal();
+		timek -= 1;
+		printf(" ");
+	}
+}
